674c81a1c0ef6aadb01b2113dfe289ca64c14348
Igor <sergienko339@gmail.com>
2024-12-23 12:36:56 -0800

    fix: rollback of the unsupported app.config.js (deprecated since friday)

--- modules/common/constants/api.ts
+++ modules/common/constants/api.ts
@@ -2,6 +2,6 @@ import Constants from 'expo-constants';
 
 const expoConstants = Constants.expoConfig?.extra ?? {};
 
-export const API_URL = expoConstants.api_url;
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const AUTH_TOKEN_KEY = 'auth_token';
