1964d69bb53b7e20f86002a00b0bad70aecbaa89
Cheastear <56431973+Cheastear@users.noreply.github.com>
2025-01-13 15:37:51 +0200

    fix: add button

--- app/restaurant/[id]/(menu)/item/[itemId]/index.tsx
+++ app/restaurant/[id]/(menu)/item/[itemId]/index.tsx
@@ -2,21 +2,24 @@ import Wrapper from "@/modules/common/components/Wrapper";
 import { Item } from "@/modules/menu/components/Item/components/Item";
 import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
+import { Platform } from "react-native";
 import { FAB, Title } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function ItemPage() {
   const { itemId, id } = useLocalSearchParams<{ itemId: string; id: string }>();
+  const insets = useSafeAreaInsets();
 
   return (
     <Wrapper>
-        <Item />
+      <Item />
       <FAB
         icon="pencil-outline"
         style={{
           position: "absolute",
           margin: 16,
           right: 0,
-          bottom: 0,
+          bottom: Platform.select({ ios: insets.bottom * 2.5, default: 0 }),
         }}
         onPress={() =>
           router.push({
