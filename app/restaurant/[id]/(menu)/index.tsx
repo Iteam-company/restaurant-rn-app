9c0e1dce3f8bb9cbe3f6f7eac04498606d868701
Cheastear <56431973+Cheastear@users.noreply.github.com>
2025-01-13 15:37:51 +0200

    fix: add button

--- app/restaurant/[id]/(menu)/index.tsx
+++ app/restaurant/[id]/(menu)/index.tsx
@@ -1,9 +1,13 @@
 import Wrapper from "@/modules/common/components/Wrapper";
 import { MenuList } from "@/modules/menu/components/MenuList";
-import { router} from "expo-router";
+import { router } from "expo-router";
+import { Platform } from "react-native";
 import { FAB } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function MenuPage() {
+  const insets = useSafeAreaInsets();
+
   return (
     <Wrapper>
       <MenuList />
@@ -13,7 +17,7 @@ export default function MenuPage() {
           position: "absolute",
           margin: 16,
           right: 0,
-          bottom: 0,
+          bottom: Platform.select({ ios: insets.bottom * 2.5, default: 0 }),
         }}
         onPress={() => router.push("./addMenu")}
       />
