e138e045a6a149ebe81354af7b6386c0239457ec
Cheastear <56431973+Cheastear@users.noreply.github.com>
2025-01-14 12:00:14 +0200

    fix: add form ui

--- modules/menu/components/MenuDetails/components/AddMenuItem/AddMenuItem.tsx
+++ modules/menu/components/MenuDetails/components/AddMenuItem/AddMenuItem.tsx
@@ -14,9 +14,13 @@ import {
   useCreateMenuItemMutation,
 } from "@/modules/menu/redux/slices/menu-api";
 import { RTKMutationPayloadType } from "@/modules/common/types";
+import { ScrollView } from "react-native";
+import getScrollViewUiSettings from "@/modules/common/constants/getScrollViewUiSettings.ios";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export const AddMenuItem = () => {
   const { menuId } = useLocalSearchParams<{ menuId: string }>();
+  const insets = useSafeAreaInsets();
 
   const [createMenu, { isLoading: isCreating }] =
     useCreateMenuItemMutation<RTKMutationPayloadType>();
@@ -47,86 +51,88 @@ export const AddMenuItem = () => {
     });
 
   return (
-    <FormWrapper>
-      <Headline>Add New Menu Item</Headline>
-      <TextInput
-        mode="outlined"
-        label="Menu item"
-        value={values.name}
-        onChangeText={(text) => setFieldValue("name", text)}
-        onBlur={handleBlur("name")}
-        error={touched.name && !!errors.name}
-        left={<TextInput.Icon icon="food" />}
-      />
+    <ScrollView style={[{ width: "100%" }, getScrollViewUiSettings(insets)]}>
+      <FormWrapper>
+        <Headline>Add New Menu Item</Headline>
+        <TextInput
+          mode="outlined"
+          label="Menu item"
+          value={values.name}
+          onChangeText={(text) => setFieldValue("name", text)}
+          onBlur={handleBlur("name")}
+          error={touched.name && !!errors.name}
+          left={<TextInput.Icon icon="food" />}
+        />
 
-      <TextInput
-        mode="outlined"
-        label="Description"
-        value={values.description}
-        onChangeText={(text) => setFieldValue("description", text)}
-        onBlur={handleBlur("description")}
-        error={touched.description && !!errors.description}
-        left={<TextInput.Icon icon="text-box-outline" />}
-      />
+        <TextInput
+          mode="outlined"
+          label="Description"
+          value={values.description}
+          onChangeText={(text) => setFieldValue("description", text)}
+          onBlur={handleBlur("description")}
+          error={touched.description && !!errors.description}
+          left={<TextInput.Icon icon="text-box-outline" />}
+        />
 
-      <TextInput
-        mode="outlined"
-        label="Ingredients"
-        value={values.ingredients}
-        onChangeText={(text) => setFieldValue("ingredients", text)}
-        onBlur={handleBlur("ingredients")}
-        error={touched.ingredients && !!errors.ingredients}
-        left={<TextInput.Icon icon="shopping" />}
-      />
+        <TextInput
+          mode="outlined"
+          label="Ingredients"
+          value={values.ingredients}
+          onChangeText={(text) => setFieldValue("ingredients", text)}
+          onBlur={handleBlur("ingredients")}
+          error={touched.ingredients && !!errors.ingredients}
+          left={<TextInput.Icon icon="shopping" />}
+        />
 
-      <TextInput
-        mode="outlined"
-        label="Time For Cook"
-        value={values.timeForCook}
-        onChangeText={(text) => setFieldValue("timeForCook", text)}
-        onBlur={handleBlur("timeForCook")}
-        error={touched.timeForCook && !!errors.timeForCook}
-        left={<TextInput.Icon icon="clock-outline" />}
-      />
+        <TextInput
+          mode="outlined"
+          label="Time For Cook"
+          value={values.timeForCook}
+          onChangeText={(text) => setFieldValue("timeForCook", text)}
+          onBlur={handleBlur("timeForCook")}
+          error={touched.timeForCook && !!errors.timeForCook}
+          left={<TextInput.Icon icon="clock-outline" />}
+        />
 
-      <TextInput
-        mode="outlined"
-        label="Price"
-        value={values.price?.toString() ?? ""}
-        onChangeText={(text) => {
-          const numericValue = text.replace(/[^0-9].[^0-9]/g, "");
-          setFieldValue("price", numericValue);
-        }}
-        onBlur={handleBlur("price")}
-        error={touched.price && !!errors.price}
-        keyboardType="numeric"
-        left={<TextInput.Icon icon="currency-usd" />}
-      />
+        <TextInput
+          mode="outlined"
+          label="Price"
+          value={values.price?.toString() ?? ""}
+          onChangeText={(text) => {
+            const numericValue = text.replace(/[^0-9].[^0-9]/g, "");
+            setFieldValue("price", numericValue);
+          }}
+          onBlur={handleBlur("price")}
+          error={touched.price && !!errors.price}
+          keyboardType="numeric"
+          left={<TextInput.Icon icon="currency-usd" />}
+        />
 
-      <TextInput
-        mode="outlined"
-        label="Weight (gram)"
-        value={values.weight?.toString() ?? ""}
-        onChangeText={(text) => {
-          const numericValue = text.replace(/[^0-9]/g, "");
-          setFieldValue("weight", numericValue);
-        }}
-        onBlur={handleBlur("weight")}
-        error={touched.weight && !!errors.weight}
-        keyboardType="numeric"
-        left={<TextInput.Icon icon="scale" />}
-      />
+        <TextInput
+          mode="outlined"
+          label="Weight (gram)"
+          value={values.weight?.toString() ?? ""}
+          onChangeText={(text) => {
+            const numericValue = text.replace(/[^0-9]/g, "");
+            setFieldValue("weight", numericValue);
+          }}
+          onBlur={handleBlur("weight")}
+          error={touched.weight && !!errors.weight}
+          keyboardType="numeric"
+          left={<TextInput.Icon icon="scale" />}
+        />
 
-      <Button mode="contained-tonal" onPress={() => handleSubmit()}>
-        {isConnecting || isCreating ? (
-          <ActivityIndicator animating={true} color={"#7c8ebf"} />
-        ) : (
-          "Add New Item"
-        )}
-      </Button>
-      <Button mode="elevated" onPress={() => router.back()}>
-        Back
-      </Button>
-    </FormWrapper>
+        <Button mode="contained-tonal" onPress={() => handleSubmit()}>
+          {isConnecting || isCreating ? (
+            <ActivityIndicator animating={true} color={"#7c8ebf"} />
+          ) : (
+            "Add New Item"
+          )}
+        </Button>
+        <Button mode="elevated" onPress={() => router.back()}>
+          Back
+        </Button>
+      </FormWrapper>
+    </ScrollView>
   );
 };
--- modules/restaurant/components/RestaurantInfo/addWorker/AddWorker.tsx
+++ modules/restaurant/components/RestaurantInfo/addWorker/AddWorker.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useFormik } from "formik";
-import { ActivityIndicator, StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet, ScrollView } from "react-native";
 import { Button, TextInput, Text, Title } from "react-native-paper";
 import FormWrapper from "@/modules/common/components/FormWrapper";
 import { useLocalSearchParams, useRouter } from "expo-router";
@@ -12,10 +12,13 @@ import {
 } from "@/modules/common/utils/createUserSchema";
 import { useSignupMutation } from "@/modules/auth/redux/slices/auth-api";
 import { useAddWorkerMutation } from "@/modules/restaurant/redux/slices/restaurant-api";
+import getScrollViewUiSettings from "@/modules/common/constants/getScrollViewUiSettings.ios";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function AddWorker() {
   const router = useRouter();
   const { id } = useLocalSearchParams<{ id: string }>();
+  const insets = useSafeAreaInsets();
 
   const [signUp, { isLoading, error }] =
     useSignupMutation<RTKMutationPayloadType>();
@@ -49,91 +52,93 @@ export default function AddWorker() {
       },
     });
   return (
-    <FormWrapper>
-      <Title>Create and add new User</Title>
-      <TextInput
-        mode="outlined"
-        label="Username"
-        value={values.username}
-        onChangeText={(text) => setFieldValue("username", text)}
-        onBlur={handleBlur("username")}
-        error={touched.username && !!errors.username}
-        left={<TextInput.Icon icon="account" />}
-      />
-      <TextInput
-        mode="outlined"
-        label="First Name"
-        value={values.firstName}
-        onChangeText={(text) => setFieldValue("firstName", text)}
-        onBlur={handleBlur("firstName")}
-        error={touched.firstName && !!errors.firstName}
-        left={<TextInput.Icon icon="account" />}
-      />
-      <TextInput
-        mode="outlined"
-        label="Last Name"
-        value={values.lastName}
-        onChangeText={(text) => setFieldValue("lastName", text)}
-        onBlur={handleBlur("lastname")}
-        error={touched.lastName && !!errors.lastName}
-        left={<TextInput.Icon icon="account" />}
-      />
-      <TextInput
-        mode="outlined"
-        label="Phone Number"
-        value={values.phoneNumber}
-        onChangeText={(text) => setFieldValue("phoneNumber", text)}
-        onBlur={handleBlur("phoneNumber")}
-        error={touched.phoneNumber && !!errors.phoneNumber}
-        keyboardType="phone-pad"
-        left={<TextInput.Icon icon="phone" />}
-      />
-      <TextInput
-        mode="outlined"
-        label="Email"
-        value={values.email}
-        onChangeText={(text) => setFieldValue("email", text)}
-        onBlur={handleBlur("email")}
-        error={touched.email && !!errors.email}
-        keyboardType="email-address"
-        autoCapitalize="none"
-        left={<TextInput.Icon icon="email" />}
-      />
-      <TextInput
-        mode="outlined"
-        label="Password"
-        value={values.password}
-        onChangeText={(text) => setFieldValue("password", text)}
-        onBlur={handleBlur("password")}
-        error={touched.password && !!errors.password}
-        secureTextEntry={!showPassword}
-        left={<TextInput.Icon icon="lock" />}
-        right={
-          <TextInput.Icon
-            icon={!showPassword ? "eye-off" : "eye"}
-            onPress={() => setShowPassword(!showPassword)}
-          />
-        }
-      />
+    <ScrollView style={getScrollViewUiSettings(insets)}>
+      <FormWrapper>
+        <Title>Create and add new User</Title>
+        <TextInput
+          mode="outlined"
+          label="Username"
+          value={values.username}
+          onChangeText={(text) => setFieldValue("username", text)}
+          onBlur={handleBlur("username")}
+          error={touched.username && !!errors.username}
+          left={<TextInput.Icon icon="account" />}
+        />
+        <TextInput
+          mode="outlined"
+          label="First Name"
+          value={values.firstName}
+          onChangeText={(text) => setFieldValue("firstName", text)}
+          onBlur={handleBlur("firstName")}
+          error={touched.firstName && !!errors.firstName}
+          left={<TextInput.Icon icon="account" />}
+        />
+        <TextInput
+          mode="outlined"
+          label="Last Name"
+          value={values.lastName}
+          onChangeText={(text) => setFieldValue("lastName", text)}
+          onBlur={handleBlur("lastname")}
+          error={touched.lastName && !!errors.lastName}
+          left={<TextInput.Icon icon="account" />}
+        />
+        <TextInput
+          mode="outlined"
+          label="Phone Number"
+          value={values.phoneNumber}
+          onChangeText={(text) => setFieldValue("phoneNumber", text)}
+          onBlur={handleBlur("phoneNumber")}
+          error={touched.phoneNumber && !!errors.phoneNumber}
+          keyboardType="phone-pad"
+          left={<TextInput.Icon icon="phone" />}
+        />
+        <TextInput
+          mode="outlined"
+          label="Email"
+          value={values.email}
+          onChangeText={(text) => setFieldValue("email", text)}
+          onBlur={handleBlur("email")}
+          error={touched.email && !!errors.email}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          left={<TextInput.Icon icon="email" />}
+        />
+        <TextInput
+          mode="outlined"
+          label="Password"
+          value={values.password}
+          onChangeText={(text) => setFieldValue("password", text)}
+          onBlur={handleBlur("password")}
+          error={touched.password && !!errors.password}
+          secureTextEntry={!showPassword}
+          left={<TextInput.Icon icon="lock" />}
+          right={
+            <TextInput.Icon
+              icon={!showPassword ? "eye-off" : "eye"}
+              onPress={() => setShowPassword(!showPassword)}
+            />
+          }
+        />
 
-      <Text style={styles.errorText}>
-        {error?.status === 401 && `Failed to sign up, please try later`}
-      </Text>
+        <Text style={styles.errorText}>
+          {error?.status === 401 && `Failed to sign up, please try later`}
+        </Text>
 
-      <Button
-        mode="elevated"
-        onPress={() => {
-          handleSubmit();
-        }}
-        style={styles.button}
-      >
-        {isLoading ? (
-          <ActivityIndicator animating={true} color={"#7c8ebf"} />
-        ) : (
-          "Create User"
-        )}
-      </Button>
-    </FormWrapper>
+        <Button
+          mode="elevated"
+          onPress={() => {
+            handleSubmit();
+          }}
+          style={styles.button}
+        >
+          {isLoading ? (
+            <ActivityIndicator animating={true} color={"#7c8ebf"} />
+          ) : (
+            "Create User"
+          )}
+        </Button>
+      </FormWrapper>
+    </ScrollView>
   );
 }
 
