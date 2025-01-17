f9d40c1d7f3b60f2e2b2e486fa7687747f40c312
Cheastear <56431973+Cheastear@users.noreply.github.com>
2025-01-17 16:08:03 +0200

    fix: menuItems FAB view + create restaurant image uploading

--- modules/common/constants/getFabUiSettings.ios.ts
+++ modules/common/constants/getFabUiSettings.ios.ts
@@ -1,9 +1,16 @@
 import { Platform } from "react-native";
 import { EdgeInsets } from "react-native-safe-area-context";
 
-const getFabUiSettings = (insets: EdgeInsets) => {
+type Options = {
+  isFABGroup: boolean;
+};
+
+const getFabUiSettings = (insets: EdgeInsets, options?: Options) => {
+  const { isFABGroup = false } = options || {};
   return Platform.select({
-    ios: { bottom: insets.bottom + 49 },
+    ios: {
+      bottom: insets.bottom + 49 + (isFABGroup && insets.top !== 20 ? -34 : 0),
+    },
     default: { bottom: 0 },
   });
 };
--- modules/restaurant/components/CreateRestaurant/utils.ts
+++ modules/restaurant/components/CreateRestaurant/utils.ts
@@ -1,11 +1,12 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export const validationSchema = Yup.object().shape({
-  restaurantName: Yup.string().required('Restaurant name is required'),
-  address: Yup.string().required('Address is required'),
+  restaurantName: Yup.string().required("Restaurant name is required"),
+  address: Yup.string().required("Address is required"),
 });
 
 export const initialValues = {
-  restaurantName: '',
-  address: '',
+  restaurantName: "",
+  address: "",
+  image: undefined,
 };
