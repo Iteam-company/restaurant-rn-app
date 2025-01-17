0836c8c887dab8905cc58b5a905edc92dfae3945
Cheastear <56431973+Cheastear@users.noreply.github.com>
2025-01-17 14:51:29 +0200

    fix: uploading images from gallery + add editing restaurant

---
+++ app/restaurant/[id]/edit.tsx
@@ -0,0 +1,13 @@
+import Wrapper from "@/modules/common/components/Wrapper";
+import EditRestaurant from "@/modules/restaurant/components/EditRestaurant/EditRestaurant";
+import React from "react";
+
+const EditRestaurantPage = () => {
+  return (
+    <Wrapper centered paddingOff>
+      <EditRestaurant />
+    </Wrapper>
+  );
+};
+
+export default EditRestaurantPage;
