cfd3d2b6d64a1c72de23698c71d76e59644be6e7
Cheastear <56431973+Cheastear@users.noreply.github.com>
2025-01-20 11:52:27 +0200

    add questions list

--- modules/common/redux/utils/api-config.ts
+++ modules/common/redux/utils/api-config.ts
@@ -2,6 +2,7 @@ export const TagTypes = {
   USER: "User",
   RESTAURANT: "Restaurant",
   QUIZ: "Quiz",
+  QUESTION: "Question",
 } as const;
 
 export type TagType = (typeof TagTypes)[keyof typeof TagTypes];
---
+++ modules/questions/components/QuestionList.tsx
@@ -0,0 +1,35 @@
+import getScrollViewUiSettings from "@/modules/common/constants/getScrollViewUiSettings.ios";
+import React from "react";
+import { View, Text, FlatList, StyleSheet, ScrollView } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useGetQuestionsQuery } from "../redux/slices/question-api";
+import { useGlobalSearchParams } from "expo-router";
+import QuestionItem from "./QuestionItem.tsx/QuestionItem";
+import { ActivityIndicator } from "react-native-paper";
+
+const QuestionList = () => {
+  const insets = useSafeAreaInsets();
+  const { id: restaurantId, quizId } = useGlobalSearchParams();
+  const { data, isLoading, error } = useGetQuestionsQuery(quizId.toString());
+
+  return (
+    <ScrollView style={[{ width: "100%" }, getScrollViewUiSettings(insets)]}>
+      <View style={styles.container}>
+        {isLoading ? (
+          <ActivityIndicator size="large" />
+        ) : (
+          data?.map((elem) => <QuestionItem key={elem.id} question={elem} />)
+        )}
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 16,
+    gap: 16,
+  },
+});
+
+export default QuestionList;
