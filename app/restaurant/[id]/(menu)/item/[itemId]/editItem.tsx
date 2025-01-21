5b27dd484d340890c80334d2dbb992f0a7e522d4
Cheastear <56431973+Cheastear@users.noreply.github.com>
2025-01-21 15:29:40 +0200

    feature: add edit item

--- app/restaurant/[id]/(menu)/item/[itemId]/editItem.tsx
+++ app/restaurant/[id]/(menu)/item/[itemId]/editItem.tsx
@@ -1,14 +1,11 @@
 import Wrapper from "@/modules/common/components/Wrapper";
-import { EditItem } from "@/modules/menu/components/Item/components/EditItem";
+import EditItem from "@/modules/menu/components/Item/components/EditItem/EditItem";
 import React from "react";
-import { Title } from "react-native-paper";
 
 export default function EditItemPage() {
   return (
-    <Wrapper>
-      <Title>
-        <EditItem />
-      </Title>
+    <Wrapper centered paddingOff>
+      <EditItem />
     </Wrapper>
   );
 }
