ac87fbf9677aedc409be66b3b215b0067ee2c035
Cheastear <56431973+Cheastear@users.noreply.github.com>
2025-01-23 14:09:26 +0200

    add quiz page

---
+++ app/user-dashboard/[id]/(quiz)/[quizId]/(questions)/index.tsx
@@ -0,0 +1,11 @@
+import Wrapper from "@/modules/common/components/Wrapper";
+import QuestionList from "@/modules/questions/components/QuestionList";
+import { Title } from "react-native-paper";
+
+export default function UserQuizItemsPage() {
+  return (
+    <Wrapper paddingOff>
+      <Title>To Do quiz</Title>
+    </Wrapper>
+  );
+}
